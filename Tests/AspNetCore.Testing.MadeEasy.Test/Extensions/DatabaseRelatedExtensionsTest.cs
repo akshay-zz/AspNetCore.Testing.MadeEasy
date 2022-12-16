21465ded7af4aad8ff32f7e32c32773b502d719b
Akshay Kumar <akshaykumar01010@gmail.com>
2022-12-16 12:40:39 +0530

    Add test case for mock http client factory

--- Tests/AspNetCore.Testing.MadeEasy.Test/Extensions/DatabaseRelatedExtensionsTest.cs
+++ Tests/AspNetCore.Testing.MadeEasy.Test/Extensions/DatabaseRelatedExtensionsTest.cs
@@ -1,23 +1,23 @@
 ﻿using AspNetCore.Testing.MadeEasy.Extensions;
 using AspNetCore.Testing.MadeEasy.UnitTest;
 
-namespace AspNetCore.Testing.MadeEasy.Test.Extensions
+namespace AspNetCore.Testing.MadeEasy.Test.Extensions;
+
+public class DatabaseRelatedExtensionsTest
 {
-    public class DatabaseRelatedExtensionsTest
+    [Fact]
+    public void Clear_should_remove_all_data()
     {
-        [Fact]
-        public void Clear_should_remove_all_data()
-        {
-            var blog = new Blog();
-            var blog2 = new Blog();
-            var data = new List<Blog> { blog, blog2, new Blog() };
-            var set = MockDb.CreateDbSet<Blog>(data);
+        var blog = new Blog();
+        var blog2 = new Blog();
+        var data = new List<Blog> { blog, blog2, new Blog() };
+        var set = MockDb.CreateDbSet<Blog>(data);
 
-            set.Object.Clear();
+        set.Object.Clear();
 
-            var result = set.Object.ToList();
+        var result = set.Object.ToList();
 
-            Assert.Empty(result);
-        }
+        Assert.Empty(result);
     }
 }
+
--- Tests/AspNetCore.Testing.MadeEasy.Test/Extensions/LoggerTestExtensionTest.cs
+++ Tests/AspNetCore.Testing.MadeEasy.Test/Extensions/LoggerTestExtensionTest.cs
@@ -2,35 +2,34 @@
 using Microsoft.Extensions.Logging;
 using Moq;
 
-namespace AspNetCore.Testing.MadeEasy.Test.Extensions
+namespace AspNetCore.Testing.MadeEasy.Test.Extensions;
+
+public class LoggerTestExtensionTest
 {
-    public class LoggerTestExtensionTest
+    [Fact]
+    public void Test()
     {
-        [Fact]
-        public void Test()
-        {
-            var logger = new Mock<ILogger<TestLogger>>();
+        var logger = new Mock<ILogger<TestLogger>>();
 
-            _ = new TestLogger(logger.Object);
+        _ = new TestLogger(logger.Object);
 
-            logger.VerifyLogging("Test information", LogLevel.Information);
-            logger.VerifyLogging("Test warning", LogLevel.Warning);
-            logger.VerifyLogging("Some error", LogLevel.Error, Times.Exactly(2));
-            logger.VerifyLogging("Debug", LogLevel.Debug, Times.Exactly(2), false);
-        }
+        logger.VerifyLogging("Test information", LogLevel.Information);
+        logger.VerifyLogging("Test warning", LogLevel.Warning);
+        logger.VerifyLogging("Some error", LogLevel.Error, Times.Exactly(2));
+        logger.VerifyLogging("Debug", LogLevel.Debug, Times.Exactly(2), false);
     }
+}
 
-    public class TestLogger
+public class TestLogger
+{
+    public TestLogger(ILogger<TestLogger> logger)
     {
-        public TestLogger(ILogger<TestLogger> logger)
-        {
-            logger.LogInformation("Test information");
-            logger.LogWarning("Test warning");
+        logger.LogInformation("Test information");
+        logger.LogWarning("Test warning");
 
-            logger.LogError("Some error");
-            logger.LogError("Some error");
+        logger.LogError("Some error");
+        logger.LogError("Some error");
 
-            logger.LogDebug("Debug detail");
-        }
+        logger.LogDebug("Debug detail");
     }
 }
