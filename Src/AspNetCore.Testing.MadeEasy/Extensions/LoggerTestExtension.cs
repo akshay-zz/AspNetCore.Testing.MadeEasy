4f6c2aeb0c3a4edad0a31f7a1c2c9426b3bcb202
Akshay Kumar <akshaykumar01010@gmail.com>
2022-12-17 01:37:58 +0530

    Fix src

---
+++ Src/AspNetCore.Testing.MadeEasy/Extensions/LoggerTestExtension.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+
+namespace AspNetCore.Testing.MadeEasy.Extensions;
+
+/// <summary>
+/// Extension for Moq logger
+/// </summary>
+public static class LoggerTestExtension
+{
+    /// <summary>
+    /// Extension method for verifying ILogger calls
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="logger"></param>
+    /// <param name="expectedMessage"></param>
+    /// <param name="expectedLogLevel"></param>
+    /// <param name="times"></param>
+    /// <param name="exactMessage"></param>
+    /// <returns></returns>
+    public static Mock<ILogger<T>> VerifyLogging<T>(this Mock<ILogger<T>> logger,
+        string expectedMessage, LogLevel expectedLogLevel, Times? times = null, bool exactMessage = true)
+    {
+        times ??= Times.Once();
+
+        Func<object, Type, bool> state = (v, t) =>
+        {
+            return exactMessage
+            ? v.ToString()!.CompareTo(expectedMessage) == 0
+            : v.ToString()!.Contains(expectedMessage);
+        };
+
+        logger.Verify(
+               x => x.Log(
+         It.Is<LogLevel>(l => l == expectedLogLevel),
+         It.IsAny<EventId>(),
+         It.Is<It.IsAnyType>((v, t) => state(v, t)),
+         It.IsAny<Exception>(),
+         It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)!));
+        return logger;
+    }
+
+}
---
+++ Src/AspNetCore.Testing.MadeEasy/IntegrationTest/TestBase.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Testing.MadeEasy.IntegrationTest;
+
+/// <summary>
+/// Inherit to your test class to get access of db context and test http client. 
+/// For each test case that run through <see cref="RunTest(Func{TDbContext, Task}, Func{HttpClient, Task}, Func{TDbContext, Task}, Func{TDbContext, Task}, bool)"/>
+/// have an independetent <see cref="HttpClient"/> and <see cref="WebApplicationFactory{TEntryPoint}"/>. Although 
+/// uses same database under the hood.
+/// </summary>
+/// <typeparam name="TDbContext">Database context class</typeparam>
+/// <typeparam name="TEntryPoint">Entry point of the application</typeparam>
+public class TestBase<TDbContext, TEntryPoint>
+    where TDbContext : DbContext
+    where TEntryPoint : class
+
+{
+    /// <summary>
+    /// Use to populate data or to clean data independet of test case.
+    /// </summary>
+    /// <param name="through"></param>
+    /// <returns></returns>
+    protected async Task ManageDb(Func<TDbContext, Task> through)
+    {
+        var application = new WebApplicationFactory<TEntryPoint>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(ConfigureServices);
+                });
+
+        using var scope = application.Services.CreateScope();
+
+        _ = application.CreateClient();
+
+        var ctx = scope.ServiceProvider.GetRequiredService<TDbContext>();
+
+        if (through != null)
+        {
+            await through(ctx);
+        }
+    }
+
+    /// <summary>
+    /// Use to run test cases.
+    /// </summary>
+    /// <param name="populatedb"></param>
+    /// <param name="test"></param>
+    /// <param name="validateDb"></param>
+    /// <param name="cleanDb"></param>
+    /// <param name="addAuth"></param>
+    /// <returns></returns>
+    protected async Task RunTest(Func<TDbContext, Task> populatedb = null,
+        Func<HttpClient, Task> test = null,
+        Func<TDbContext, Task> validateDb = null,
+        Func<TDbContext, Task> cleanDb = null, bool addAuth = true)
+    {
+        try
+        {
+            var application = new WebApplicationFactory<TEntryPoint>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(ConfigureServices);
+                });
+            var client = application.CreateClient();
+            using var scope = application.Services.CreateScope();
+
+            var ctx = scope.ServiceProvider.GetRequiredService<TDbContext>();
+
+            await this.SetUp(ctx);
+
+            if (populatedb != null)
+                await populatedb(ctx);
+
+            if (addAuth)
+                this.AddAuthenticationToClient(client);
+
+            if (test != null)
+                await test(client);
+
+            if (validateDb != null)
+                await validateDb(ctx);
+
+            if (cleanDb != null)
+                await cleanDb(ctx);
+
+            await this.TearDown(ctx);
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+
+    }
+
+    /// <summary>
+    /// Add authentication to the http client. Overide to extend client authentication
+    /// </summary>
+    /// <param name="client"></param>
+    protected virtual void AddAuthenticationToClient(HttpClient client)
+    {
+        //Add authentication
+    }
+
+    /// <summary>
+    /// Setup services for the test cases. Overide to customise services. Override to configure the database connection string
+    /// </summary>
+    /// <param name="services"></param>
+    protected virtual void ConfigureServices(IServiceCollection services)
+    {
+        var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<TDbContext>));
+
+        services.Remove(descriptor!);
+
+        // Need to add if will handle the authentication
+
+        //services.AddAuthorization(config =>
+        //{
+        //});
+    }
+
+    /// <summary>
+    /// Will be called before every test case.
+    /// </summary>
+    /// <param name="ctx"></param>
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+    protected virtual async Task SetUp(TDbContext ctx)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+    {
+        //Never add any code here. Only to be used by overriding.
+    }
+
+    /// <summary>
+    /// Will be called after every test case.
+    /// </summary>
+    /// <param name="ctx"></param>
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+    protected virtual async Task TearDown(TDbContext ctx)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+    {
+        //Never add any code here. Only to be used by overriding.
+    }
+}
